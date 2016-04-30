8571918e04595636b7bd1d7cb31979021e4d029f
David Neal <davelneal@att.net>
2016-04-30 17:30:29 -0500

    First Commit base level

---
+++ Projects.vb
@@ -0,0 +1,10 @@
+﻿Imports Microsoft.VisualBasic
+
+Public Class Project
+    'This Class will build the basic componets of Daily, Weekly or Monthly Projects that I wish to do around the house
+    Dim daily As String
+    Dim weekly As String
+    Dim monthly As String
+
+
+End Class
