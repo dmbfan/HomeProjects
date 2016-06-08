b5f564d80c49935c408ca7737a29d83d9bb650df
David Neal <davelneal@att.net>
2016-06-07 21:21:16 -0500

    Button to get to local is working.  Trying to fix the listbox exceptions

--- DailyNotes/Form1.vb
+++ DailyNotes/Form1.vb
@@ -1,32 +1,32 @@
 ﻿Public Class Form1
 
     Private Sub btnLivingRoom_Click(sender As Object, e As EventArgs) Handles btnLivingRoom.Click
-        Me.Close()
+        Me.Hide()
         LivingRoom.Show()
     End Sub
 
     Private Sub btnBonus_Click(sender As Object, e As EventArgs) Handles btnBonus.Click
-        Me.Close()
+        Me.Hide()
         BonusRoom.Show()
     End Sub
 
     Private Sub btnYard_Click(sender As Object, e As EventArgs) Handles btnYard.Click
-        Me.Close()
+        Me.Hide()
         Yard.Show()
     End Sub
 
     Private Sub btnBoys_Click(sender As Object, e As EventArgs) Handles btnBoys.Click
-        Me.Close()
+        Me.Hide()
         BoysRoom.Show()
     End Sub
 
     Private Sub btnMaster_Click(sender As Object, e As EventArgs) Handles btnMaster.Click
-        Me.Close()
+        Me.Hide()
         Master.Show()
     End Sub
 
     Private Sub btnKitchen_Click(sender As Object, e As EventArgs) Handles btnKitchen.Click
-        Me.Close()
+        Me.Hide()
         Kitchen.Show()
     End Sub
 End Class
